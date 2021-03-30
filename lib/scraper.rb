a5a4fd01acb614fd0d1330e867b6055eb69166dd
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-29 20:47:38 -0400

    commit tree 2b73ce9d43305915ac5deea593959a1d2e004923

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,17 @@
+#require 'pry'
+
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html) 
+courses = doc.css(".tout__label.heading.heading--level-4") 
+
+courses.each do |course|
+    puts course.text.strip
+  end 
+
+
+  p doc.css(".tout__label.heading.heading--level-4")[0].name
+  #p doc.css(".tout__label.heading.heading--level-4")[0].attributes
